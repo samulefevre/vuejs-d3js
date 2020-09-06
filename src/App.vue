edca34a8acbc60aa0a6d81d1a8351f26f7bbc400
Samuel Lefèvre <samuel.lefevr@gmail.com>
2020-09-06 17:48:18 +0200

    create PackChart component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <PackChart :tweetData="loadData" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PackChart from './components/PackChart.vue'
+
+import * as d3 from 'd3'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PackChart
+  },
+  data() {
+    return {
+      loadData: {}
+    }
+  },
+  methods: {
+    async fecthData() {
+      let data = await d3.json('./data.json')
+      console.log('data')
+      console.log(data)
+      this.loadData = data
+    }
+  },
+  mounted() {
+    console.log('App loaded')
+    this.fecthData()
   }
 }
 </script>
